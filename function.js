dd40f92d49906b17e336e8da8ada90465075278a
tanvir1538 <tkabir1111@gmail.com>
2025-08-29 19:25:59 +0600

    finally added clear button works and finally done all funtionality

--- function.js
+++ function.js
@@ -27,7 +27,7 @@ const copyButtons = document.getElementsByClassName("copy_button");
 
 for (const copyButton of copyButtons) {
     copyButton.addEventListener("click", function () {
-        const par_element = copyButton.closest('.shadow-lg'); 
+        const par_element = copyButton.closest('.shadow-lg');
         const copyNumber = par_element.querySelector(".card-number").innerText;
         const copyCount = parseInt(document.getElementById("copyCount").innerText);
 
@@ -43,7 +43,65 @@ for (const copyButton of copyButtons) {
     });
 }
 
+// call and clear funtionality 
+
+const history = [];
+
+const callButtons = document.getElementsByClassName('callButton');
+for (const callButton of callButtons) {
+    callButton.addEventListener('click', function () {
+        const coinCount = parseInt(document.getElementById("point-count").innerText);
+        const callerID = callButton.parentElement.parentElement;
+        const service = callerID.querySelector(".service-name").innerText;
+        const card_number = callerID.querySelector(".card-number").innerText;
+
+        if (coinCount >= 20) {
+            alert(`calling ${service} ${card_number}...`);
+            const newCoinCount = coinCount - 20;
+            document.getElementById("point-count").innerText = newCoinCount;
+            const storeHistory = document.getElementById("call-History");
+
+
+            const callHistory = {
+                name: service,
+                number: card_number,
+                time: new Date().toLocaleTimeString()
+
+            };
+
+            history.push(callHistory);
 
 
 
+            const div = document.createElement('div');
+            div.innerHTML = `
+                <div class ="bg-[#FAFAFA] hind-madurai-font flex justify-between items-center my-4 p-4 rounded-[8px]">
+                    <div>
+                    <h1 class="inter-font font-semibold text-[18px]">${callHistory.name}</h1>
+                    <h1 class="text-[18px] text-gray-500 ">${callHistory.number}</h1>
+                </div>
+                <div>
+                    <h1 class="text-[18px] ">${callHistory.time}</h1>
+                </div>
+                </div>
+            `;
+            storeHistory.appendChild(div);
 
+        } else {
+            alert("I have not  enough coin to make a call");
+        }
+
+
+
+    });
+
+}
+
+
+// clear option 
+document.getElementById("clearButton").addEventListener("click", function () {
+    const storeHistory = document.getElementById("call-History");
+    storeHistory.innerText = " ";
+})
+
+        
